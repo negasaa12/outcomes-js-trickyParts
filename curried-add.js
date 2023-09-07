53914a7f65dc565bc9a8812728c9915802ba516d
kelly <sntsklvn@gmail.com>
2023-09-07 13:01:46 -0400

    one test fail for curried-add  but i was told it was due to export of the files but it does work

--- curried-add.js
+++ curried-add.js
@@ -1,7 +1,16 @@
-function curriedAdd() {
-    
+function curriedAdd(initial = 0) {
+    let total = initial;
+  
+    function add(num) {
+      if (typeof num === "undefined") {
+        return total;
+      }
+      total += num;
+      return add;
+    }
+  
+    return add;
   }
   
-
 
 module.exports = { curriedAdd };
