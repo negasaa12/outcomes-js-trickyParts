1b0383d97f13142a02ae032277daeeb006edeb22
kelly <sntsklvn@gmail.com>
2023-09-08 09:46:13 -0400

    there is no mock-random provided by the downloaded code. So the test are failing

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,24 @@
 function guessingGame() {
-    
+    const secretNumber = Math.floor(Math.random() * 100);
+    let numberOfGuesses = 0;
+    let hasWon = false;
+  
+    return function guess(number) {
+      if (hasWon) {
+        return "The game is over, you already won!";
+      }
+      
+      numberOfGuesses++;
+      
+      if (number < secretNumber) {
+        return `${number} is too low!`;
+      } else if (number > secretNumber) {
+        return `${number} is too high!`;
+      } else {
+        hasWon = true;
+        return `You win! You found ${secretNumber} in ${numberOfGuesses} guesses.`;
+      }
+    };
   }
 
 module.exports = { guessingGame };
